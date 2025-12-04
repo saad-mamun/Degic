5fa30c94711d8b04d3c01f0ed9d596d83ac40eae
saad-mamun <mamunali88991@gmail.com>
2025-12-05 01:01:41 +0600

    Add Video component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Presentation from './components/Presentation';
 import Slider from './components/Slider';
-import Video from './components/video';
+import Video from './components/Videos';
 import Time from './components/Time';
 
 
